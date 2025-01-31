cd3ee4b54a64fb3c8cba91a7cbb116162ebf2f85
Diego Garces <thebroyt.290@gmail.com>
2025-01-31 04:30:37 -0600

    Sleep Screen

--- Scripts/Socket.js
+++ Scripts/Socket.js
@@ -1,53 +1,27 @@
 // request.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Obtenemos los parámetros de la URL actual
-    const params = new URLSearchParams(window.location.search);
-    const device = params.get("device");
-    const tag = params.get("tag");
+    const device = 'l34t0r1z4d0r53cr3t0';
+    const tag = '73929203';
 
-    // Opcional: podrías mostrar los valores obtenidos en consola
-    console.log("Device:", device);
-    console.log("Tag:", tag);
+    socket = io('https://serverws-gwch.onrender.com');
+    socket.emit('registerDevice', { device, tag });
 
-    // Hacemos la solicitud al archivo PHP que se encargará de consultar la base de datos
-    // Supongamos que se llama "consulta.php" (cámbialo al nombre que desees)
-    fetch(`../Back-End/consulta.php?device=${device}&tag=${tag}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log("Respuesta del servidor:", data);
-
-            if (data.success) {
-                socket = io(data.message);
-
-                socket.emit('registerDevice', { device, tag });
-
-                socket.on('accesoConcedido', (data) => {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Acceso concedido',
-                        text: 'Tu dispositivo ha sido autenticado correctamente.'
-                    });
-                });
-                socket.on('accesoDenegado', (data) => {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Acceso denegado',
-                        text: 'Alerta: alguien está intentando acceder sin tu consentimiento al sistema.'
-                    });
-                });
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Acceso denegado',
-                    text: data.message
-                });
-            }
-        })
-        .catch((error) => {
-            Swal.fire({
-                icon: 'error',
-                title: 'Acceso denegado',
-                text: error
-            });
+    socket.on('accesoConcedido', (data) => {
+        Swal.fire({
+            icon: 'success',
+            title: 'Acceso concedido',
+            text: 'Tu dispositivo ha sido autenticado correctamente.',
+            timer: 2000
         });
+        window.location.href = "../DashBoard/";
+    });
+    socket.on('accesoDenegado', (data) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Acceso denegado',
+            text: 'Alerta: alguien está intentando acceder sin tu consentimiento al sistema.',
+            timer: 2000
+        });
+    });
+
 });
