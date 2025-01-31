7a02afeb16185604d42d627102fabec349b06f07
Diego Garces <thebroyt.290@gmail.com>
2025-01-31 04:34:05 -0600

    Session Check

---
+++ Back-End/session_check.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if (isset($_SESSION['usuario'])) {
+    echo json_encode([
+        'success' => true,
+        'usuario' => $_SESSION['usuario']
+    ]);
+} else {
+    echo json_encode([
+        'success' => false,
+        'message' => 'No hay sesión activa'
+    ]);
+}
+?>
